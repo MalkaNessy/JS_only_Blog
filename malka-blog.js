cfee3201cbf0313e8ef01a8021ffac30020799d7
Malka <malka.nessy@gmail.com>
2017-05-08 19:58:41 +0300

    save_edited_post

--- malka-blog.js
+++ malka-blog.js
@@ -42,6 +42,18 @@ this.data = {0: {id:0, type: "post", text: "post1", title: "title post 1", comme
 		return posts;
 	}
 	
+	//возвращает список всех комментов к данному посту
+	this.comments_to_post = function (post_id)
+	{
+		var comments_list = [];
+		for (key in this.postList[post_id].comments)
+		{
+			comments_list.push (this.postList[post_id].comments[key]);
+		}
+		return comments_list;
+	}
+	
+	//обновляет максимальное количество постов (нужно для добавления id к следующему посту)
 	this.update_max_post_id = function () //вызывается из model.load_data 
 	{
 		console.log('model.update_max_post_id start');
@@ -57,6 +69,7 @@ this.data = {0: {id:0, type: "post", text: "post1", title: "title post 1", comme
 		this.max_post_id = max_;
 	}
 	
+	//обновляет максимальное количество комментариев (нужно для добавления id к следующему комментарию)
 	this.update_max_comment_id = function (post_id) //
 	{
 		console.log('model.update_max_comment_id start');
@@ -76,21 +89,35 @@ this.data = {0: {id:0, type: "post", text: "post1", title: "title post 1", comme
 	this.save_post = function () 
 	{
 		console.log('model.save_post start');
-			var new_post_id = this.max_post_id+1;
-			var input_title = $("#title").val();
-			var input_text = $("#textarea").val();
-			var new_post = {id: new_post_id, type: "post", text: input_text, title: input_title, comments: {}};
+		var new_post_id = this.max_post_id+1;
+		var input_title = $("#title").val();
+		var input_text = $("#textarea").val();
+		var new_post = {id: new_post_id, type: "post", text: input_text, title: input_title, comments: {}};
 						
-			this.data[new_post_id]=new_post;
-			//console.log("this.data[new_post_id]: " + this.data[new_post_id]);
-			this.max_post_id=new_post_id;
+		this.data[new_post_id]=new_post;
+		this.max_post_id=new_post_id;
 			
-			this.postList = this.posters_for_all_pages();
-			//console.log("save_post new data: " + this.data);
-			//console.log("save_post postList: " + this.postList);
-			$("#new_root_post_form").html("");
-		    screen_.show_posts_from_root(model.postList);
+		this.postList = this.posters_for_all_pages();
 			
+		$("#new_root_post_form").html("");
+		screen_.show_posts_from_root(model.postList);
+			
+	}
+	
+	//сохранение отредактированного поста
+	this.save_edited_post = function(post_id)
+	{
+		console.log('model.save_edited_post start');
+		console.log('post_id: ' + post_id);
+		console.log('model.data ['+ post_id+'].text: ' + model.data [post_id].text);
+		var input_title = $("#title").val();
+		var input_text = $("#textarea").val();
+		model.data [post_id].title = input_title;
+		model.data [post_id].text = input_text;
+		console.log('model.data ['+ post_id+'].text: ' + model.data [post_id].text);
+		
+		$(".post_text").html("");
+		screen_.show_posts_from_root(model.postList);
 	}
 	
 	//удаление поста, принимает id поста
@@ -137,7 +164,6 @@ function Screen ()
 	this.show_posts_from_root = function (postList)
 	{
 		console.log("screen.show_posts_from_root start, postList: " + postList);
-		
 		var title = "";
 		var title_html="";
 		var posts_html="";
@@ -154,32 +180,23 @@ function Screen ()
 			}
 			if (postList[i].comments)
 			{
-				var comments_list = [];
-				for (key in postList[i].comments)
-				{
-					comments_list.push (postList[i].comments[key]);
-				}
-				
-				
+				var comments_list = model.comments_to_post(i);
 				var comments_html ="";
 				for (var key = comments_list.length-1; key >= 0; key--)//key in postList[i].comments
-				{
-					
+				{	
 					console.log("show_posts for, postList[ " + i + "], key: " + key);
 					var comment = postList[i].comments[key].text;
 					console.log("postList[ " + i + "].comments[" + key + "].text: " + comment);
 					var comment_id = postList [i].comments[key].id;
 					//console.log("show_posts for, postList[ " + i + "].comments[" + key + "].id: " + comment_id);
-					comments_html += '<div class="comments'+comment_id+'">'+comment+'</div>';
-					
+					comments_html += '<div class="comments" id="comment'+i+comment_id+'">'+comment+'</div>';
 					
 				}
-				
 			}
 			
 			title_html = '<div class="title">' + title + '</div>';
 		
-		    posts_html += title_html +
+		    posts_html +='<div class="post" id="post'+postList[i].id+'">'+ title_html +
 						'<div class="post_text">'+ postList[i].text + '</div>'+
 						'<div class="buttons">'+
 						  '<div class="button edit_button" id="edit'+postList[i].id+'" onclick="edit_click('+postList[i].id+');">Edit </div>'+
@@ -187,12 +204,13 @@ function Screen ()
 						  '<div class="button comment_button" id="comment'+postList[i].id+'" onclick="comment_click('+postList[i].id+', \'new_post_form'+postList[i].id+'\');">Comment</div>'+
 					   '</div>'+
 						'<div class="new_post_form" id="new_post_form'+postList[i].id+'">'+
-						comments_html+'</div>';
+						comments_html+'</div></div>';
 			//console.log("postList[i].comments.length:"+postList[i].comments);
 
 	    }
 		console.log("show_posts_from_root end");
-		$("#posts").html (posts_html);
+		
+		$("#posts").html ( posts_html);
 		
 	}
 		
@@ -220,7 +238,7 @@ function Screen ()
 		$("#new_post_form"+post_id).html( input_title_html +
 						  '<textarea id="textarea" name="post"></textarea>'+//why need name
 						  '<div class="buttons">'+
-						  '<div class="button" onclick="save_comment_click('+post_id+');">Save</div>'+
+						  '<div class="button" onclick="save_comment_click();">Save</div>'+
 						  '<div class="button" onclick="hide_comment_form();">Cancel</div>'+
 						  '</div>');
 		$("#textarea").cleditor();
@@ -233,6 +251,30 @@ function Screen ()
 		console.log("screen.hide_form start");
 		$(".new_post_form").html("");
 	}
+	
+	//открыть форму редактирования поста
+	this.show_edit_form = function (post_id)
+	{
+		console.log("screen.show_edit_form start, post_id: " + post_id);
+		var post = model.data [post_id];
+		console.log("model.data [post_id]: " + model.postList [post_id]); 
+		 
+		
+		$("#post"+post.id+'>.post_text').html('<textarea id="textarea" name="post">'+post.text+'</textarea>'+
+							'<div class="buttons">'+
+							'<div class="button" class="save_button" onclick="save_post_click('+post_id+');">Save</div>'+
+							'<div class="button" class="cancel_button" onclick="cancel_click();">Cancel</div>'+
+							'</div>');
+		
+		$("#post"+post.id+'>.title').html('<input type="text" name="title" id="title", value="'+post.title+'">');
+		
+	  $("#textarea").cleditor();
+	  //this.update_buttons();
+	}
+	
+	
+	
+	
 };
 
 function add_new_post_click ()
@@ -242,14 +284,27 @@ function add_new_post_click ()
     
 }
 
-function save_post_click ()
+function save_post_click (post_id)
 {
 	console.log("save_post_click start");
 	if ($("#textarea").val()||$("#title").val())
 	{
-		model.save_post();
+		if(post_id!==undefined)
+		{
+			model.save_edited_post(post_id);
+		}
+		
+		//model.save_post();
 		//screen_.hide_form_show_posts();
 	}
+}
+
+function edit_click (post_id)
+{
+	console.log("edit_click start");
+	//model.to_edit_mode (post_id);
+	screen_.show_edit_form (post_id);
+	
 }
 
 function cancel_click ()
